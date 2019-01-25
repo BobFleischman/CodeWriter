0f9700861d9d4127fc1e7cb356feba629110e85f
Bob Fleischman <RFleischman@duanemorris.com>
2019-01-24 20:57:52 -0500

    made the form pretty

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -3,6 +3,7 @@ import { ItemObject } from '../models/item-object';
 import { Field } from '../models/field';
 import { Types } from '../models/types.enum';
 import { InputTypes } from '../models/input-types.enum';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 
 @Component({
@@ -15,25 +16,88 @@ export class GeneratorComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.item = new ItemObject();
-    this.item.name = 'Person';
-    this.item.label = 'firstData';
-    let f = new Field('first', Types.STRING, 'First Name', '', InputTypes.INPUT);
-    f.required = true;
-    f.placeholder = 'Enter Nickname if applicable';
-    this.item.fields.push(f);
-    this.item.fields.push(
-      new Field('last', Types.STRING, 'Last Name', '', InputTypes.INPUT)
-    );
-    f = new Field('admin', Types.BOOLEAN, 'Administrator', '', InputTypes.CHECKBOX);
-    f.required = true;
-    this.item.fields.push(f);
-    this.item.fields.push(
-      new Field('birthdate', Types.DATE, 'Birthday', '', InputTypes.DATE)
-    );
-    this.item.fields.push(
-      new Field('comment', Types.STRING, 'Comments', '', InputTypes.TEXTAREA)
-    );
+    this.item = JSON.parse(`{
+      "fields": [{
+        "placeholder": "Field Name",
+        "required": true,
+        "name": "name",
+        "type": "string",
+        "label": "Field Name",
+        "validations": "",
+        "formFieldType": 0
+      }, {
+        "placeholder": "",
+        "required": true,
+        "name": "label",
+        "type": "string",
+        "label": "Label for Forms",
+        "validations": "",
+        "formFieldType": 0
+      }, {
+        "placeholder": "text to user in the placehold attribute",
+        "required": false,
+        "name": "placeholder",
+        "type": "string",
+        "label": "Placeholder Text",
+        "validations": "",
+        "formFieldType": 0
+      }, {
+        "placeholder": "Is this field required on the form",
+        "required": true,
+        "name": "required",
+        "type": "boolean",
+        "label": "Required",
+        "validations": "",
+        "formFieldType": 5
+      }, {
+        "placeholder": "string, Date, number, boolean",
+        "required": true,
+        "name": "type",
+        "type": "string",
+        "label": "Data Type",
+        "validations": "",
+        "formFieldType": 0
+      }, {
+        "placeholder": "Validation String (Not currently used)",
+        "required": false,
+        "name": "validations",
+        "type": "string",
+        "label": "Validations",
+        "validations": "",
+        "formFieldType": 0
+      }, {
+        "placeholder": "INPUT, TEXTAREA, DATE, SELECT, PASSWORD, CHECKBOX",
+        "required": true,
+        "name": "formFieldType",
+        "type": "string",
+        "label": "Form Field Type",
+        "validations": "",
+        "formFieldType": 0
+      }],
+      "name": "ObjectTemplate",
+      "label": "Object Template"
+    }
+
+    `);
+    // this.item = new ItemObject();
+    // this.item.name = 'Person';
+    // this.item.label = 'firstData';
+    // let f = new Field('first', Types.STRING, 'First Name', '', InputTypes.INPUT);
+    // f.required = true;
+    // f.placeholder = 'Enter Nickname if applicable';
+    // this.item.fields.push(f);
+    // this.item.fields.push(
+    //   new Field('last', Types.STRING, 'Last Name', '', InputTypes.INPUT)
+    // );
+    // f = new Field('admin', Types.BOOLEAN, 'Administrator', '', InputTypes.CHECKBOX);
+    // f.required = true;
+    // this.item.fields.push(f);
+    // this.item.fields.push(
+    //   new Field('birthdate', Types.DATE, 'Birthday', '', InputTypes.DATE)
+    // );
+    // this.item.fields.push(
+    //   new Field('comment', Types.STRING, 'Comments', '', InputTypes.TEXTAREA)
+    // );
   }
 
   f1() {}
@@ -87,53 +151,26 @@ export class GeneratorComponent implements OnInit {
   }
   generateSubmitCode(): string {
     return `
-    submitCode() {
+    onSubmit(data) {
       console.log('do something here');
     }
     `;
   }
 
-  generateHTMLCodeV1(obj: ItemObject): string {
-    let result = '';
-    const top = `<h2>${obj.label}</h2>
-    <form [formGroup]='dataform' (ngSubmit)='onSubmit(userform.value)'>
-    <p-panel header='${obj.name}'>
-        <div class='ui-grid ui-grid-responsive ui-grid-pad ui-fluid' style='margin: 10px 0px'>\n`;
-  const buttonRow = `           <div class='ui-grid-row'>
-<div class='ui-grid-col-2'></div>
-<div class='ui-grid-col-6'>
-    <button pButton type='submit' label='Submit' [disabled]='!dataform.valid'></button>
-</div>
-<div class='ui-grid-col-4'></div>
-</div>`;
-    const bottom = `        </div>
-    </p-panel>
-</form>\n`;
-    obj.fields.forEach(f => {
-      // console.log(this.generateHTMLCodeRow(f));
-      result = result + this.generateHTMLCodeRow(f);
-    });
-    result = top + result + buttonRow + bottom;
-    console.log('XXXXXXXXXXXXXXXXX: ' + result);
-    // var re = /[\n]/g;
-    // result = result.replace(re, '<br \>');
-    return result;
-  }
-
   generateHTMLCode(obj: ItemObject): string {
     let result = '';
     const top = `<h2>${obj.label}</h2>
-    <form [formGroup]='dataform' (ngSubmit)='onSubmit(userform.value)'>
+    <form [formGroup]='dataform' (ngSubmit)='onSubmit(dataform.value)'>
     <p-panel header='${obj.name}'>
-        <div class='ui-grid 'p-responsive 'p-pad ui-fluid' style='margin: 10px 0px'>\n`;
-  const buttonRow = `           <div class='p-row'>
+`;
+  const buttonRow = `           <div class='p-grid'>
 <div class='p-col-2'></div>
 <div class='p-col-6'>
     <button pButton type='submit' label='Submit' [disabled]='!dataform.valid'></button>
 </div>
 <div class='p-col-4'></div>
 </div>`;
-    const bottom = `        </div>
+    const bottom = `
     </p-panel>
 </form>\n`;
     obj.fields.forEach(f => {
@@ -147,9 +184,9 @@ export class GeneratorComponent implements OnInit {
     return result;
   }
   generateHTMLCodeRow(f: Field): string {
-    const line = `<div class='p-row'>
+    const line = `<div class='p-grid'>
     <div class='p-col-2'>
-        ${f.label} *:
+        ${this.getInputLabel(f)}
     </div>
     <div class='p-col-6'>` + this.getInputType(f) + `
 
@@ -161,30 +198,23 @@ export class GeneratorComponent implements OnInit {
     return line;
   }
 
-  generateHTMLCodeRowV1(f: Field): string {
-    const line = `<div class='ui-grid-row'>
-    <div class='ui-grid-col-2'>
-        ${f.label} *:
-    </div>
-    <div class='ui-grid-col-6'>` + this.getInputType(f) + `
-
-    </div>
-    <div class='ui-grid-col-4'>
-        <p-message severity='error' text='${f.label} is required' *ngIf="!dataform.controls['${f.name}'].valid&&dataform.controls['${f.name}'].dirty"></p-message>
-    </div>
-</div>\n`;
-    return line;
+  getInputLabel(f: Field): string {
+    let result = f.label;
+    if (f.required) {
+      result = result + ' *';
+    }
+    return result + ':';
   }
   getInputType(f: Field): string {
     const result = '';
     const ty = f.formFieldType;
     switch (ty) {
       case InputTypes.INPUT: {
-        return `<input pInputText type='text' formControlName='${f.name}' placeholder='${f.placeholder}'/>`;
+        return `<input pInputText class='ui-fluid ui-inputtext' type='text' formControlName='${f.name}' placeholder='${f.placeholder}'/>`;
         break;
       }
       case InputTypes.PASSWORD: {
-        return `<input pInputText type='password' formControlName='${f.name}' placeholder='${f.placeholder}'/>`;
+        return `<input pInputText class='ui-fluid ui-inputtext' type='password' formControlName='${f.name}' placeholder='${f.placeholder}'/>`;
         break;
       }
       case InputTypes.CHECKBOX: {
@@ -192,7 +222,7 @@ export class GeneratorComponent implements OnInit {
         break;
       }
       case InputTypes.TEXTAREA: {
-        return `<textarea formControlName='${f.name}' rows='5' cols='30' pInputTextarea autoResize='autoResize'></textarea>`;
+        return `<textarea class='ui-fluid ui-inputtext' formControlName='${f.name}' rows='5' cols='30' pInputTextarea autoResize='autoResize'></textarea>`;
         break;
       }
       case InputTypes.DATE: {
@@ -205,5 +235,12 @@ export class GeneratorComponent implements OnInit {
       }
     }
     return result;
+  }
+
+  getCSS() {
+    return `.ui-inputtext, .ui-button {
+      width: 100%;
+    }
+    `;
   }
 }
