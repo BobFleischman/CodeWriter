d16483dfaa7f47b3fbd03d116061ecd00cacb135
Bob Fleischman <RFleischman@duanemorris.com>
2019-01-26 21:57:51 -0500

    Added mask to fields

--- src/app/models/field.ts
+++ src/app/models/field.ts
@@ -2,25 +2,30 @@ import { Types } from './types.enum';
 import { InputTypes } from './input-types.enum';
 
 export class Field {
-    name: string;
+    name: string = '';
     type: Types; // should be enumeration number, string, boolean, date. any
-    label: string;
+    label: string = '';
+    mask: string = '';
     placeholder = '';
     required = false;
-    regularexpression: string;
-    regularexpressionmsg: string;
-    validations: string;
+    regularexpression: string = '';
+    regularexpressionmsg: string = '';
+    validations: string = '';
     formFieldType: InputTypes;
-
-    constructor(n: string, t: Types, l: string, v: string, fft: InputTypes) {
-        this.name = n;
-        this.type = t;
-        this.label = l;
-        this.validations = v;
-        this.formFieldType = fft;
-    }
 
     showJSON() {
         console.log(JSON.stringify(this));
     }
+
+
+}
+
+export function fieldconstructor(n: string, t: Types, l: string, v: string, fft: InputTypes) {
+    let f = new Field();
+    f.name = n;
+    f.type = t;
+    f.label = l;
+    f.validations = v;
+    f.formFieldType = fft;
+    return f;
 }
